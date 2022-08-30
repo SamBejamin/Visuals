bfe2c984f46dd0a8ef77fc55d0d3943908046f63
SamBejamin <111352752+SamBejamin@users.noreply.github.com>
2022-08-30 16:38:25 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,13 @@
 import * as React from "react";
 import { Box, Container } from "@mui/material";
 import VisChartToggle from "../src/components/VisChartToggle";
-import React, { useState, useRef } from "react";
-import Typography from "@mui/material/Typography";
-import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
-import css from "./UploadBox.module.css";
 
 export default function Index() {
   return (
     <Container maxWidth="lg">
       <Box sx={{ my: 6 }}>
         <VisChartToggle />
-        <p>Home Page</p> 
-        <div className={css.uploadBox}>
-          <DriveFolderUploadIcon />
-          <Typography variant="h6">Upload Data</Typography>
-        </div>
+        <p>Home Page</p>
       </Box>
     </Container>
   );
